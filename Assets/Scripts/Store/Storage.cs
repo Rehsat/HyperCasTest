5cee73e268f470e0f0cbba97bfff4f5550707f1b
Rehsat <alesandrgorshkov@mail.ru>
2023-04-11 22:18:43 +0300

    Сделал размещение на полки

--- Assets/Scripts/Store/Storage.cs
+++ Assets/Scripts/Store/Storage.cs
@@ -1,19 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Storage : MonoBehaviour
+public class Storage : MonoBehaviour, IStorablesContainer
 {
+    // TODO: Попытаться разделить на MVC
     [SerializeField] private bool _isHorizontal;
     [SerializeField] private int _maxStorablesInStorage;
     [SerializeField] private int _maxInRow;
     [SerializeField] private Vector3 _spacing;
     [SerializeField] private Transform _startStoragePostion;
 
+    private Vector3 _standartSpacing;
+    private Vector3 _rowOverflowSpacing;
+
+    private Vector3 _lastStorablePlacePosition;
+    
     private Stack<Storable> _storables = new Stack<Storable>();
     
     public int CurrentStorablesCount => _storables.Count;
-    
+
+    private void Start()
+    {
+        
+        _standartSpacing = _isHorizontal ? 
+            new Vector3(_spacing.x, 0, 0) : new Vector3(0, _spacing.y, 0);
+        
+        _rowOverflowSpacing = _isHorizontal ? 
+            new Vector3(0, _spacing.y, 0) : new Vector3(_spacing.x, 0, 0);
+    }
+
     public void AddStorable(Storable storable)
     {
         storable.transform.parent = _startStoragePostion;
@@ -28,11 +45,30 @@ public class Storage : MonoBehaviour
 
     public Vector3 GetNextStoragePosition()
     {
-        return Vector3.zero + _spacing * _storables.Count;
+        var rowNumber = _storables.Count / _maxInRow;
+       // var lastStorablePlacePosition = _storables.Count > 0 ? 
+         //   _storables.Peek().transform.localPosition : Vector3.zero;
+
+        var nextPosition = Vector3.zero +
+                           (rowNumber*_rowOverflowSpacing +  _storables.Count%_maxInRow * _standartSpacing);
+        return nextPosition;
     }
 
     public int GetEmptySLotsCount()
     {
         return _maxStorablesInStorage - CurrentStorablesCount;
+    }
+
+    public List<Storable> GetStorables(int count)
+    {
+        var list = new List<Storable>();
+        for(int i =0; i <count; i++)
+        {
+            if(_storables.Count == 0) break;
+            var lastPickUpedStorable = _storables.Pop();
+            list.Add(lastPickUpedStorable);
+        }
+
+        return list;
     }
 }
